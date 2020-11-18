f24c27e283746e987625aa0910b6af6e4e61420c
Seongchuel Ahn <acidlab.help@gmail.com>
2020-11-18 06:02:35 +0000

    Test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 from messenger import Messenger
+from telegram.error import TimedOut
 
 if __name__ == '__main__':
     bot = Messenger()
@@ -7,14 +8,16 @@ if __name__ == '__main__':
     bot.sendMessage('안녕하세요. 출근했습니다.')
     bot.sendMessage('관심 키워드를 입력하시면\n관련 기사 제목을 보여드립니다.')
     bot.sendMessage('좋은 하루 보내세요.')
-    print(old)
     while True:
         bot = Messenger()
         new = bot.getMessage()
-        print(new)
         if new != old:
             response = bot.searchKeyword(new.get('message'))
-            bot.sendMessage(response)
+            try:
+                bot.sendMessage(response)
+            except TimedOut:
+                bot = Messenger()
+                bot.sendMessage(response)
             old = new
         time.sleep(1)
 
